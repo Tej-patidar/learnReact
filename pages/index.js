f82d13e4d860b6d265b79d236f50cc92689b87f3
Tarun <tarunpatidar742@gmail.com>
2023-04-22 18:11:29 +0530

    tasklist

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,42 @@
-import React, { useState } from 'react'
+import { useState } from "react";
+import { nanoid } from "nanoid";
 const index = () => {
-  const [name, setName] = useState("Tarun Patidar")
-const [date, setDate]= useState(new Date().toLocaleTimeString());
-const [fnme, setFname]= useState('Mr. Sundarlal ji')
+const [title, setTitle] = useState("")
+const [desc, setDesc] = useState("")
 
-setInterval(() => {
-  setDate(new Date().toLocaleTimeString())
-}, 1000);
-  const changeHandler=()=>{
-    console.log("Inside ChangedHandler");
-    setName('Shinudaaaa')
-  }
+const todocreate=(e)=>{
+e.preventDefault();
+// console.log(object);
+const todo={
+  id:nanoid(),
+  title,
+  desc,
 
-  const chaneFname=()=>{
-    setFname('My PitaShree')
-  }
+}
+console.log(todo);
+setTitle(''),
+setDesc('')
+}
+
+//   const changeHandler=(v)=>{
+//     console.log('changed');
+// console.log(v.target.value);
+//   }
+
   return (
-    <div>
-      <h1>{name}</h1>
-      {date}
-      <h1 onClick={chaneFname}>{fnme}</h1>
-      <button onClick={changeHandler}>Change Name</button>
+    <div className="container w-50 bg-light">
+      <form onSubmit={todocreate}>
+        <h1 className="bg-dark fw-light text-light">Create An Amazing ToDO List With Tarun</h1>
+        <input type="text" placeholder="Title" className="form-control"
+        onChange={(e)=>setTitle(e.target.value)} value={title}/><br />
+
+        <input type="text" placeholder="Desc" className="form-control" onChange={(e)=>setDesc (e.target.value)} value={desc}/><br />
+        <button className="btn btn-primary">Create Todo</button>
+      </form>
+
+      <hr />
+      <h2>Pending Task</h2>
+     <li className="d-flex justify-content-between p-4">Item 1  <i className="ri-delete-bin-2-fill text-danger"></i></li>
     </div>
   )
 }
